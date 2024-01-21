1c4d7d54cb9f0439f9f30f49a128ce7f9a161854
Tyler Jewell <tyler.p.jewell@gmail.com>
2024-01-21 12:46:33 -0700

    Refactor end-to-end tests and add test for starting the app

--- app/test/e2e_test.dart
+++ app/test/e2e_test.dart
@@ -1,4 +1,4 @@
-import 'package:app/main.dart';
+import 'package:app/main.dart' as app;
 import 'package:app/pages/pages.dart';
 import 'package:app/store.dart';
 import 'package:flutter/material.dart';
@@ -20,83 +20,88 @@ Future<void> main() async {
   // Load app fonts to ensure goldens have actual text.
   await loadAppFonts();
 
-  testWidgets('End-to-end test', (WidgetTester tester) async {
-    // Start the app
-    await tester.pumpWidget(
-      VxState(store: AppStore(), child: const VxExampleApp()),
-    );
+  group('End-to-end test', () {
+    testWidgets('start app', (widgetTester) async {
+      app.main();
+    });
+    testWidgets('End-to-end test', (WidgetTester tester) async {
+      /// Start the app
+      await tester.pumpWidget(
+        VxState(store: AppStore(), child: const app.VxExampleApp()),
+      );
 
-    /// On HomePage, take screenshot for golden test
-    await expectLater(
-      find.byType(HomePage),
-      matchesGoldenFile('goldens/home_page.png'),
-    );
+      /// On HomePage, take screenshot for golden test
+      await expectLater(
+        find.byType(HomePage),
+        matchesGoldenFile('goldens/home_page.png'),
+      );
 
-    /// Tap the Get user button and wait for 2 seconds
-    await tester.tap(find.byType(ElevatedButton));
-    await tester.pumpAndSettle(const Duration(seconds: 1));
+      /// Tap the Get user button and wait for 2 seconds
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pumpAndSettle(const Duration(seconds: 1));
 
-    /// On HomePage with data, take screenshot for golden test
-    await expectLater(
-      find.byType(HomePage),
-      matchesGoldenFile('goldens/home_page-data.png'),
-    );
+      /// On HomePage with data, take screenshot for golden test
+      await expectLater(
+        find.byType(HomePage),
+        matchesGoldenFile('goldens/home_page-data.png'),
+      );
 
-    /// Tap the question mark icon to go to the AboutPage
-    await tester.tap(find.byIcon(Icons.question_mark_sharp));
-    await tester.pumpAndSettle();
+      /// Tap the question mark icon to go to the AboutPage
+      await tester.tap(find.byIcon(Icons.question_mark_sharp));
+      await tester.pumpAndSettle();
 
-    /// On AboutPage, take screenshot for golden test
-    await expectLater(
-      find.byType(AboutPage),
-      matchesGoldenFile('goldens/about_page.png'),
-    );
+      /// On AboutPage, take screenshot for golden test
+      await expectLater(
+        find.byType(AboutPage),
+        matchesGoldenFile('goldens/about_page.png'),
+      );
 
-    /// Tap back button.
-    await tester.tap(find.byIcon(Icons.arrow_back));
-    await tester.pumpAndSettle();
+      /// Tap back button.
+      await tester.tap(find.byIcon(Icons.arrow_back));
+      await tester.pumpAndSettle();
 
-    /// On HomePage
-    expect(find.byType(HomePage), findsOneWidget);
+      /// On HomePage
+      expect(find.byType(HomePage), findsOneWidget);
 
-    /// Tap the profile tab to go to the ProfilePage
-    await tester.tap(find.byIcon(Icons.person));
-    await tester.pumpAndSettle();
+      /// Tap the profile tab to go to the ProfilePage
+      await tester.tap(find.byIcon(Icons.person));
+      await tester.pumpAndSettle();
 
-    /// On ProfilePage, take screenshot for golden test
-    await expectLater(
-      find.byType(ProfilePage),
-      matchesGoldenFile('goldens/profile_page.png'),
-    );
+      /// On ProfilePage, take screenshot for golden test
+      await expectLater(
+        find.byType(ProfilePage),
+        matchesGoldenFile('goldens/profile_page.png'),
+      );
 
-    /// Tap the ElevatedButton to go to the UpdateNamePage
-    await tester.tap(find.byType(ElevatedButton));
-    await tester.pumpAndSettle();
+      /// Tap the ElevatedButton to go to the UpdateNamePage
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pumpAndSettle();
 
-    /// On UpdateNamePage, take screenshot for golden test
-    await expectLater(
-      find.byType(UpdateNamePage),
-      matchesGoldenFile('goldens/update_name_page.png'),
-    );
+      /// On UpdateNamePage, take screenshot for golden test
+      await expectLater(
+        find.byType(UpdateNamePage),
+        matchesGoldenFile('goldens/update_name_page.png'),
+      );
 
-    /// Enter 'Test Name' into the TextFormField and tap the button
-    await tester.enterText(find.byType(TextFormField), 'Test Name');
-    await tester.pumpAndSettle();
+      /// Enter 'Test Name' into the TextFormField and tap the button
+      await tester.enterText(find.byType(TextFormField), 'Test Name');
+      await tester.pumpAndSettle();
 
-    /// On UpdateNamePage with text, take screenshot for golden test
-    await expectLater(
-      find.byType(UpdateNamePage),
-      matchesGoldenFile('goldens/update_name_page-text.png'),
-    );
+      /// On UpdateNamePage with text, take screenshot for golden test
+      await expectLater(
+        find.byType(UpdateNamePage),
+        matchesGoldenFile('goldens/update_name_page-text.png'),
+      );
 
-    /// Tap the button
-    await tester.tap(find.byType(ElevatedButton));
-    await tester.pumpAndSettle();
+      /// Tap the button
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pumpAndSettle();
 
-    /// On ProfilePage, with updated name, take screenshot for golden test
-    await expectLater(
-      find.byType(ProfilePage),
-      matchesGoldenFile('goldens/profile_page-updated.png'),
-    );
+      /// On ProfilePage, with updated name, take screenshot for golden test
+      await expectLater(
+        find.byType(ProfilePage),
+        matchesGoldenFile('goldens/profile_page-updated.png'),
+      );
+    });
   });
 }
