05cd0075832d6ca910050e659cfc8baa6cc4cae1
Tyler Jewell <tyler.p.jewell@gmail.com>
2024-01-20 21:20:19 -0700

    Refactor HomePage widget and update AppBar

--- app/lib/pages/home_page.dart
+++ app/lib/pages/home_page.dart
@@ -4,11 +4,7 @@ import 'package:app/store.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-extension on VxStatus? {
-  bool get isLoading => this == VxStatus.loading;
-}
-
-/// The home page.
+/// The HomePage
 class HomePage extends StatelessWidget {
   /// Create the home page.
   const HomePage({super.key});
@@ -16,82 +12,35 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Page')),
-      body: const HomeBody(),
-    );
-  }
-}
-
-/// The body of the home page.
-class HomeBody extends StatelessWidget {
-  /// Create the home page body.
-  const HomeBody({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return [
-      const Divider(),
-
-      /// Call to action buttons.
-      'Call to actions'.text.xl2.bold.make(),
-
-      /// hStack of action buttons
-      [
-        /// Calls the [GetUser] mutation.
+      appBar: AppBar(
+        title: 'Home page'.text.make(),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.question_mark_sharp),
+            onPressed: () => context.nextPage(const AboutPage()),
+          ).p12(),
+        ],
+      ),
+      body: [
+        /// Shows the user's name.
+        /// Listens to the [GetUser] mutation.
+        VxBuilder<AppStore>(
+          mutations: const {GetUser},
+          builder: (_, store, status) => switch (status) {
+            VxStatus.loading => 'loading...',
+            _ => store.user.name.isNotEmpty
+                ? 'data: ${store.user.name}'
+                : 'no data',
+          }
+              .text
+              .xl2
+              .make(),
+        ).p4(),
         ElevatedButton(
           onPressed: GetUser.new,
           child: 'Get user'.text.make(),
         ).p12(),
-
-        /// Calls the [SetUserAge] mutation.
-        ElevatedButton(
-          onPressed: SetUserAge.new,
-          child: 'Update user age'.text.make(),
-        ).p12(),
-      ].hStack(alignment: MainAxisAlignment.center),
-
-      const Divider(),
-
-      'User data'.text.xl2.bold.make(),
-
-      /// vStack of user data
-      [
-        /// Shows the user's name.
-        ///
-        /// Listens to the [GetUser] mutation.
-        VxBuilder(
-          builder: (_, AppStore d, s) =>
-              'name: ${s.isLoading ? 'getting user name...' : d.user.name}'
-                  .text
-                  .make(),
-          mutations: const {GetUser},
-        ).p12(),
-
-        /// Shows the user's age.
-        ///
-        /// Listens to the [GetUser] and [SetUserAge] mutations.
-        ///
-        VxBuilder(
-          builder: (_, AppStore d, s) {
-            final ageText = s.isLoading
-                ? 'getting user age...'
-                : d.user.age == 0
-                    ? ''
-                    : d.user.age;
-
-            return 'age: $ageText'.text.make();
-          },
-          mutations: const {GetUser, SetUserAge},
-        ).p12(),
-      ].vStack(),
-
-      const Divider(),
-
-      /// Go to another page.
-      ElevatedButton(
-        onPressed: () => context.nextPage(const AnotherPage()),
-        child: 'Go to another page'.text.make(),
-      ),
-    ].vStack(alignment: MainAxisAlignment.spaceAround);
+      ].vStack(alignment: MainAxisAlignment.center).centered(),
+    );
   }
 }
