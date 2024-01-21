7c9e21503e01fa933bcc8755586ab257923d1f83
Tyler Jewell <tyler.p.jewell@gmail.com>
2024-01-20 21:19:53 -0700

    Update GetUser mutation to set in-memory user and call UpdateName mutation

--- app/lib/mutations/get_user.dart
+++ app/lib/mutations/get_user.dart
@@ -10,13 +10,12 @@ class GetUser extends VxMutation<AppStore> {
     /// Simulate a delay.
     await Future<void>.delayed(const Duration(seconds: 1));
 
-    /// Set the user.
+    /// Set the in-memory user.
     store?.user = User()
-      ..age = 42
       ..id = '1234'
       ..name = 'John Doe';
 
-    /// Call the set user age which will update user's age
-    // next(SetUserAge.new);
+    /// Call the set user name which will update user's name
+    // next(UpdateName.new);
   }
 }
