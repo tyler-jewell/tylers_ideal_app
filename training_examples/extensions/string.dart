e3a11c0be0cab2b6799a35153365d8a7728a72f8
Tyler Jewell <tyler.p.jewell@gmail.com>
2024-01-21 14:36:25 -0700

    Remove unused code and add code coverage

--- training_examples/extensions/string.dart
+++
@@ -1,5 +0,0 @@
-/// https://velocityx.dev/docs/extensions/string
-///
-/// Eg: "mtechviral@gmail.com".validateEmail()
-/// Eg:  "5454545454545454".isCreditCardValid()
-library;
--- training_examples/extensions/theme.dart
+++
@@ -1,7 +0,0 @@
-/// https://velocityx.dev/docs/extensions/context#theme
-
-/// context.theme = Theme.of(context)
-/// context.textTheme = Theme.of(context).textTheme
-/// context.captionStyle = Theme.of(context).textTheme.caption
-
-library;
