664384e71ed3a4fd45a433c7438de39581c175fd
Tyler Jewell <tyler.p.jewell@gmail.com>
2024-01-21 12:03:04 -0700

    Remove delay in SetCurrentIndex mutation

--- app/lib/mutations/set_current_index.dart
+++ app/lib/mutations/set_current_index.dart
@@ -6,8 +6,6 @@ class SetCurrentIndex extends VxMutation<AppStore> {
   /// Perform the mutation
   @override
   void perform() {
-    /// Simulate a delay.
-
     /// Set the in-memory user.
     store?.currentIndex = store?.currentIndex == 0 ? 1 : 0;
   }
