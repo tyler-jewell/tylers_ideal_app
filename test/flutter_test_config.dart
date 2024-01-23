793a03dea8335e5cabbb230a05dc9873db30dfaf
Tyler Jewell <tyler.p.jewell@gmail.com>
2024-01-22 17:21:10 -0700

    Add font loading in testExecutable function

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -5,6 +5,7 @@ import 'dart:async';
 import 'package:alchemist/alchemist.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
+  await loadFonts();
   return AlchemistConfig.runWithConfig(
     config: const AlchemistConfig(
       platformGoldensConfig: PlatformGoldensConfig(
